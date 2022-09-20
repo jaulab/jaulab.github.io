fbd7846d6235249222dd4daf52c2d8bf3e752d39
jaulab <36453316+jaulab@users.noreply.github.com>
2022-09-20 16:57:43 +0200

    Noticias

---
+++ JS/buttons.js
@@ -0,0 +1,18 @@
+function saberMas(id){
+  switch(id){
+    case 'saber-Inv':
+      window.open("https://www.unavarra.es/nocheinvestiga", '_blank');
+      break;
+    case 'saber-Nak':
+      window.open("https://nakfestival.com/", '_blank');
+      break;
+    case 'saber-Semana':
+      window.open("https://www.unavarra.es/unidadculturacientifica/semana-de-la-ciencia?contentId=262160", '_blank');
+      break;
+    case 'saber-Pharus':
+      window.open("https://www.unavarra.es/jornadas-pharus", '_blank');
+      break;
+    default:
+      break;
+  }
+}
